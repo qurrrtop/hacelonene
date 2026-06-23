70b12629ed94e37b65db8e1068658cfb86145aaa
Javier Maidana <Javiermaidana1234@gmail.com>
2026-06-23 16:54:34 -0300

    agregué algunas reglas para cargar luego valores - INCOMPLETO

---
+++ src/main/webapp/assets/js/Rules.js
@@ -0,0 +1,30 @@
+export default class Rules {
+        
+        static provisions = {
+            NAME: {
+            type: "string",
+            min: 3,
+            max: 80,
+            regex: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s'-]+$/
+            },
+            LAST_NAME: {
+                type: "string",
+                min: 3,
+                max: 50,
+                regex: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s'-]+$/
+            },
+            EMAIL: {
+                type: "string",
+                min: 10,
+                max: 30,
+                regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+            },
+            PASSWORD: {
+                type: "string",
+                min: 3,
+                max: 255,
+                regex: /^[A-Za-z\d@$!%*?&]+$/
+            },
+            
+        }
+}
